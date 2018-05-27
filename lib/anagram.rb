d72c6e163c5bbde527791447ad87dfe854da17f8
Scott Silverman <scottms724@gmail.com>
2018-05-27 15:10:12 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,20 @@
-# Your code goes here!
+class Anagram
+  attr_accessor :word
+  
+  def initialize(word)
+    @word = word 
+  end
+  
+  def match(word_array)
+    matched = []
+    split_word = @word.split("")
+    word_array.collect do |w|
+      if w == split_word
+        matched << w 
+      else 
+        matched
+      end
+    end 
+  end
+  
+end 
